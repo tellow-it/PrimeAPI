a1bbc9061d6dd80aa12b0754bc7c80e80d392b32
Тихонов Иван А <iatikhonov@f-soft.org~>
2023-09-15 17:27:01 +0300

    change schema order

--- schemas/order.py
+++ schemas/order.py
@@ -34,12 +34,21 @@ def normal_prefetch(order: Order):
         'expected_time': order.expected_time,
         'description': order.description
     }
+
     if order.building:
         prefetch_order['building'] = {'id': order.building.id,
                                       'building_name': order.building.building_name}
+    elif not order.building:
+        prefetch_order['building'] = {'id': None,
+                                      'building_name': "Удалено"}
+
     if order.system:
         prefetch_order['system'] = {'id': order.system.id,
                                     'system_name': order.system.system_name}
+    elif not order.system:
+        prefetch_order['system'] = {'id': None,
+                                    'system_name': "Удалено"}
+
     if order.creator:
         prefetch_order["creator"] = {'id': order.creator.id,
                                      "name": order.creator.name,
@@ -47,11 +56,26 @@ def normal_prefetch(order: Order):
                                      "role": order.creator.role,
                                      "telephone": order.creator.telephone
                                      }
+    elif not order.creator:
+        prefetch_order["creator"] = {'id': None,
+                                     "name": "Удален",
+                                     "surname": "Удален",
+                                     "role": "Удален",
+                                     "telephone": "Удален"
+                                     }
+
     if order.important:
         prefetch_order["important"] = {'id': order.important.id,
                                        'important_name': order.important.important_name}
+    elif not order.important:
+        prefetch_order["important"] = {'id': None,
+                                       'important_name': "Удалено"}
+
     if order.status:
         prefetch_order["status"] = {'id': order.status.id,
                                     'status_name': order.status.status_name}
+    elif not order.status:
+        prefetch_order["status"] = {'id': None,
+                                    'status_name': "Удалено"}
     return prefetch_order
 
